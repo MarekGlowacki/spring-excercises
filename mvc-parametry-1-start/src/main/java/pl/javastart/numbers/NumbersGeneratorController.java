bc987cf5597441e43be075ca2c78bcab8f4aeaae
User <user@its.me>
2023-01-20 05:35:36 +0100

    czytelniejsza wersja

--- mvc-parametry-1-start/src/main/java/pl/javastart/numbers/NumbersGeneratorController.java
+++ mvc-parametry-1-start/src/main/java/pl/javastart/numbers/NumbersGeneratorController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
 @Controller
 class NumbersGeneratorController {
-
     private final NumberService numberService;
+    private static final int DEFAULT_LOWER_BOUND = 0;
+    private static final int DEFAULT_UPPER_BOUND = 1000;
+    private static final int DEFAULT_BOUND_DIFF = 100;
+    private static final String INCORRECT_PARAMS_MESSAGE =
+            "Dolne ograniczenie musi być mniejsze, bądź równe górnemu ograniczeniu";
 
     public NumbersGeneratorController(NumberService numberService) {
         this.numberService = numberService;
@@ -20,54 +22,63 @@ class NumbersGeneratorController {
     @ResponseBody
     String getRandomNumber(@RequestParam(required = false) Integer from,
                            @RequestParam(required = false) Integer to) {
-        if (from > to) {
-            return "Dolne ograniczenie musi być mniejsze lub równe górnemu ograniczeniu.";
-        } else if (from == null && to != null) {
-            from = to - 100;
-        } else if (to == null && from != null) {
-            to = from + 100;
-        } else if (from == null && to == null) {
-            from = 0;
-            to = 1000;
+        if (incorrectParams(from, to)) {
+            return INCORRECT_PARAMS_MESSAGE;
         }
-        int randomNumberResult = numberService.getRandomNumberFromRange(from, to);
-        return String.format("Losowa liczba z przedziału [%d; %d]: %d", from, to, randomNumberResult);
+        int lowerBound = getLowerBound(from, to);
+        int upperBound = getUpperBound(from, to);
+        String randomNumber = Integer.toString(numberService.getRandomNumberFromRange(lowerBound, upperBound));
+        return String.format("Losowa liczba z przedziału [%d; %d]: %s: ", lowerBound, upperBound, randomNumber);
     }
 
     @GetMapping("/even-numbers")
     @ResponseBody
     String getEvenNumbers(@RequestParam(required = false) Integer from,
                           @RequestParam(required = false) Integer to) {
-
-        if (from > to) {
-            return "Dolne ograniczenie musi być mniejsze lub równe górnemu ograniczeniu.";
-        } else if (from == null && to != null) {
-            from = to - 100;
-        } else if (to == null && from != null) {
-            to = from + 100;
-        } else if (from == null && to == null) {
-            from = 0;
-            to = 1000;
+        if (incorrectParams(from, to)) {
+            return INCORRECT_PARAMS_MESSAGE;
         }
-        List<Integer> evenNumbersResult = numberService.getEvenNumbersFromRange(from, to);
-        return String.format("Parzyste liczby z przedziału [%d; %d]: ", from, to) + evenNumbersResult;
+        int lowerBound = getLowerBound(from, to);
+        int upperBound = getUpperBound(from, to);
+        String numbers = numberService.getEvenNumbersFromRange(lowerBound, upperBound).toString();
+        return String.format("Liczby parzyste z przedziału [%d; %d]: %s", lowerBound, upperBound, numbers);
     }
 
     @GetMapping("/odd-numbers")
     @ResponseBody
     String getOddNumbers(@RequestParam(required = false) Integer from,
                          @RequestParam(required = false) Integer to) {
-        if (from > to) {
-            return "Dolne ograniczenie musi być mniejsze lub równe górnemu ograniczeniu.";
-        } else if (from == null && to != null) {
-            from = to - 100;
-        } else if (to == null && from != null) {
-            to = from + 100;
-        } else if (from == null && to == null) {
-            from = 0;
-            to = 1000;
+        if (incorrectParams(from, to)) {
+            return INCORRECT_PARAMS_MESSAGE;
         }
-        List<Integer> evenNumbersResult = numberService.getOddNumbersFromRange(from, to);
-        return String.format("Nieparzyste liczby z przedziału [%d; %d]: ", from, to) + evenNumbersResult;
+        int lowerBound = getLowerBound(from, to);
+        int upperBound = getUpperBound(from, to);
+        String numbers = numberService.getOddNumbersFromRange(lowerBound, upperBound).toString();
+        return String.format("Liczby nieparzyste z przedziału [%d; %d]: %s", lowerBound, upperBound, numbers);
     }
+
+    private int getLowerBound(Integer from, Integer to) {
+        if (from != null) {
+            return from;
+        } else if (to == null) {
+            return DEFAULT_LOWER_BOUND;
+        } else {
+            return to - DEFAULT_BOUND_DIFF;
+        }
+    }
+
+    private int getUpperBound(Integer from, Integer to) {
+        if (to != null) {
+            return to;
+        } else if (from == null) {
+            return DEFAULT_UPPER_BOUND;
+        } else {
+            return from + DEFAULT_BOUND_DIFF;
+        }
+    }
+
+    private boolean incorrectParams(Integer from, Integer to) {
+        return from != null && to != null && from > to;
+    }
+
 }
